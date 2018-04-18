bfc108b60481fe8d04960a8a4cacbb5531a04240
William J Teskey <willjteskey@gmail.com>
2018-04-18 00:52:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+
+ def countdown_with_sleep(z)
+  sleep(z)
+end
+
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    countdown_with_sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
