b98c15d15c4a51989abc3934eb390efed6a7f86b
William J Teskey <willjteskey@gmail.com>
2018-04-18 00:51:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,16 @@
 #write your code here
 
 def countdown
- 
+ def countdown_with_sleep(z)
+  sleep(z)
+end
+
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    countdown_with_sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
 end
